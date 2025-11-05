c9c275f6c132adde7daf78e490947d2d12394dd5
Joaovfcosta <54521415+Joaovfcosta@users.noreply.github.com>
2025-11-04 22:34:30 -0300

    Add files via upload

---
+++ teste/menu.php
@@ -0,0 +1,13 @@
+<?php
+    echo "<h2>Menu</h2>";
+?>
+
+<nav>
+    <ul>
+        <li><a href="?pg=principal"> Home </a></li>
+        <li><a href="?pg=quemsomos"> Quem Somos </a></li>
+        <li><a href="?pg=clientes"> Clientes </a></li>
+        <li><a href="?pg=faleconosco"> Fale Conosco </a></li>
+         <li><a href="?pg=marcaaula"> marcação de aulas </a></li>
+    </ul>
+</nav>
---
+++ teste/topo.php
@@ -0,0 +1,8 @@
+<header style="background-color: #007BFF; height: 150px">
+
+<?php
+
+    echo "<h1>Habilitando Com Sucesso</h1>";
+?>
+
+</header>
